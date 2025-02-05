26c833017518476588fafc64c8d4e92bf0b0e56a
Jobson Oliveira <jobsondeveloper@gmail.com>
2025-02-04 22:18:49 -0300

    Mudança de pasta do projeto

---
+++ src/components/Formulario.vue
@@ -0,0 +1,25 @@
+<script setup>
+    const props = defineProps(["cadastraTarefa", "tarefaTemporaria", "atualizaFiltro", "editaTarefaTemp"]);
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa">
+      <div class="row">
+        <div class="col">
+          <input type="text" class="form-control" :value="props.tarefaTemporaria" @change="editaTarefaTemp" required>
+        </div>
+
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+
+        <div class="col-md-2">
+          <select class="form-control" @change="props.atualizaFiltro">
+            <option value="todas">Todas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+</template>
---
+++ src/components/ListaTarefas.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps(["tarefas"]);
+</script>
+
+<template>
+    <ul class="list-group mt-4" v-for="tarefas in props.tarefas">
+        <li class="list-group-item">
+            <input :checked="tarefas.finalizada" :id="tarefas.titulo" type="checkbox"
+                @change="event => tarefas.finalizada = event.target.checked">
+            <label :for="tarefas.titulo" class="ms-3" :class="{ done: tarefas.finalizada }">
+                {{ tarefas.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
