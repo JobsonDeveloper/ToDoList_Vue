dbad5e3506fb1efffd54c76c755144fd6a6cb158
Jobson Oliveira <jobsondeveloper@gmail.com>
2025-02-04 21:41:47 -0300

    Projeto construido sem components

--- ToDoList/src/App.vue
+++ ToDoList/src/App.vue
@@ -1,47 +1,115 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { reactive } from "vue";
+
+const estado = reactive({
+  tarefas: [
+    {
+      titulo: "Estudar TypeScript",
+      finalizada: false,
+    },
+    {
+      titulo: "Estudar Sass",
+      finalizada: true,
+    },
+    {
+      titulo: "Estudar React.ts",
+      finalizada: false,
+    },
+    {
+      titulo: "Estudar Spring Boot",
+      finalizada: true,
+    }
+  ],
+  filtro: "todas",
+  tarefaTemporaria: "",
+});
+
+function atualizaFiltro(e) {
+  estado.filtro = e.target.value;
+}
+
+// function toggleChecked(e) {
+//   tarefas.finalizada = e.target.checked;
+// }
+
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter(tarefas => !tarefas.finalizada);
+}
+
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter(tarefas => tarefas.finalizada);
+}
+
+const getTarefas = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case "pendentes":
+      return getTarefasPendentes();
+      break;
+    case "finalizadas":
+      return getTarefasFinalizadas();
+      break;
+    default:
+      return estado.tarefas;
+  }
+}
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemporaria,
+    finalizada: false
+  }
+  estado.tarefas.push(tarefaNova);
+  estado.tarefaTemporaria = "";
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>
+        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
+      </p>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <form @submit.prevent="cadastraTarefa">
+      <div class="row">
+        <div class="col">
+          <input type="text" class="form-control" :value="estado.tarefaTemporaria" @change="event => estado.tarefaTemporaria = event.target.value" required>
+        </div>
 
-  <main>
-    <TheWelcome />
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+
+        <div class="col-md-2">
+          <select class="form-control" @change="atualizaFiltro">
+            <option value="todas">Todas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+
+    <ul class="list-group mt-4" v-for="tarefas in getTarefas()">
+      <li class="list-group-item">
+        <input :checked="tarefas.finalizada" :id="tarefas.titulo" type="checkbox" @change="event => tarefas.finalizada = event.target.checked">
+        <label :for="tarefas.titulo" class="ms-3" :class="{ done: tarefas.finalizada }">
+          {{ tarefas.titulo }}
+        </label>
+      </li>
+    </ul>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+  text-decoration: line-through;
 }
 </style>
